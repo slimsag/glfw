a16864169a25614fef0eea80234d3fd6f66bcd31
Stephen Gutekanst <stephen@hexops.com>
2023-06-29 01:26:31 -0700

    better upstream merge strategy
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- verify.sh
+++ verify.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-git diff $(git merge-base origin/master upstream/master)..origin/master \
+git diff $(git merge-base master upstream/master)..master \
     --diff-filter=d \
     ':(exclude)README.md' \
     ':(exclude)build.zig' \
     ':(exclude)build.zig.zon' \
+    ':(exclude)update.sh' \
+    ':(exclude)verify.sh' \
     ':(exclude).github' \
     ':(exclude).gitattributes' \
     ':(exclude).gitignore'
