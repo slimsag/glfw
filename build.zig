77fa6c7dc17601c8c8525d364afddc2f7bd129aa
Stephen Gutekanst <stephen@hexops.com>
2023-08-08 20:35:01 -0700

    workaround transitive dependency issue
    Helps hexops/mach#902
    
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- build.zig
+++ build.zig
@@ -4,6 +4,137 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    // TODO: uncomment this once hexops/mach#902 is fixed.
+    // b.dependency may not be called inside pub fn build if we want to use this package via the
+    // package manager transitively.
+    _ = target;
+    _ = optimize;
+
+    // const shared = b.option(bool, "shared", "Build as a shared library") orelse false;
+
+    // const use_x11 = b.option(bool, "x11", "Build with X11. Only useful on Linux") orelse true;
+    // const use_wl = b.option(bool, "wayland", "Build with Wayland. Only useful on Linux") orelse true;
+
+    // const use_opengl = b.option(bool, "opengl", "Build with OpenGL; deprecated on MacOS") orelse false;
+    // const use_gles = b.option(bool, "gles", "Build with GLES; not supported on MacOS") orelse false;
+    // const use_metal = b.option(bool, "metal", "Build with Metal; only supported on MacOS") orelse true;
+
+    // const lib = std.Build.CompileStep.create(b, .{
+    //     .name = "glfw",
+    //     .kind = .lib,
+    //     .linkage = if (shared) .dynamic else .static,
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+
+    // lib.linkLibC();
+
+    // if (shared) {
+    //     lib.defineCMacro("_GLFW_BUILD_DLL", "1");
+    // }
+
+    // lib.installHeadersDirectory("include/GLFW", "GLFW");
+
+    // lib.linkLibrary(b.dependency("vulkan_headers", .{
+    //     .target = target,
+    //     .optimize = optimize,
+    // }).artifact("vulkan-headers"));
+
+    // if (lib.target_info.target.os.tag == .macos) {
+    //     // MacOS: this must be defined for macOS 13.3 and older.
+    //     // Critically, this MUST NOT be included as a -D__kernel_ptr_semantics flag. If it is,
+    //     // then this macro will not be defined even if `defineCMacro` was also called!
+    //     lib.defineCMacro("__kernel_ptr_semantics", "");
+    //     @import("xcode_frameworks").addPaths(b, lib);
+    // }
+
+    // const include_src_flag = "-Isrc";
+
+    // switch (lib.target_info.target.os.tag) {
+    //     .windows => {
+    //         lib.linkSystemLibraryName("gdi32");
+    //         lib.linkSystemLibraryName("user32");
+    //         lib.linkSystemLibraryName("shell32");
+
+    //         if (use_opengl) {
+    //             lib.linkSystemLibraryName("opengl32");
+    //         }
+
+    //         if (use_gles) {
+    //             lib.linkSystemLibraryName("GLESv3");
+    //         }
+
+    //         const flags = [_][]const u8{ "-D_GLFW_WIN32", include_src_flag };
+    //         lib.addCSourceFiles(&base_sources, &flags);
+    //         lib.addCSourceFiles(&windows_sources, &flags);
+    //     },
+    //     .macos => {
+    //         lib.linkSystemLibraryName("objc");
+    //         lib.linkFramework("IOKit");
+    //         lib.linkFramework("CoreFoundation");
+    //         lib.linkFramework("AppKit");
+    //         lib.linkFramework("CoreServices");
+    //         lib.linkFramework("CoreGraphics");
+    //         lib.linkFramework("Foundation");
+
+    //         if (use_metal) {
+    //             lib.linkFramework("Metal");
+    //         }
+
+    //         if (use_opengl) {
+    //             lib.linkFramework("OpenGL");
+    //         }
+
+    //         const flags = [_][]const u8{ "-D_GLFW_COCOA", include_src_flag };
+    //         lib.addCSourceFiles(&base_sources, &flags);
+    //         lib.addCSourceFiles(&macos_sources, &flags);
+    //     },
+
+    //     // everything that isn't windows or mac is linux :P
+    //     else => {
+    //         var sources = std.BoundedArray([]const u8, 64).init(0) catch unreachable;
+    //         var flags = std.BoundedArray([]const u8, 16).init(0) catch unreachable;
+
+    //         sources.appendSlice(&base_sources) catch unreachable;
+    //         sources.appendSlice(&linux_sources) catch unreachable;
+
+    //         if (use_x11) {
+    //             lib.linkLibrary(b.dependency("x11_headers", .{
+    //                 .target = target,
+    //                 .optimize = optimize,
+    //             }).artifact("x11-headers"));
+
+    //             sources.appendSlice(&linux_x11_sources) catch unreachable;
+    //             flags.append("-D_GLFW_X11") catch unreachable;
+    //         }
+
+    //         if (use_wl) {
+    //             lib.linkLibrary(b.dependency("wayland_headers", .{
+    //                 .target = target,
+    //                 .optimize = optimize,
+    //             }).artifact("wayland-headers"));
+
+    //             lib.defineCMacro("WL_MARSHAL_FLAG_DESTROY", "1");
+
+    //             sources.appendSlice(&linux_wl_sources) catch unreachable;
+    //             flags.append("-D_GLFW_WAYLAND") catch unreachable;
+    //             flags.append("-Wno-implicit-function-declaration") catch unreachable;
+    //         }
+
+    //         flags.append(include_src_flag) catch unreachable;
+
+    //         lib.addCSourceFiles(sources.slice(), flags.slice());
+    //     },
+    // }
+    // b.installArtifact(lib);
+}
+
+// TODO: remove this once hexops/mach#902 is fixed.
+pub fn lib(
+    b: *std.Build,
+    optimize: std.builtin.OptimizeMode,
+    target: std.zig.CrossTarget,
+) *std.Build.Step.Compile {
     const shared = b.option(bool, "shared", "Build as a shared library") orelse false;
 
     const use_x11 = b.option(bool, "x11", "Build with X11. Only useful on Linux") orelse true;
@@ -13,7 +144,7 @@ pub fn build(b: *std.Build) void {
     const use_gles = b.option(bool, "gles", "Build with GLES; not supported on MacOS") orelse false;
     const use_metal = b.option(bool, "metal", "Build with Metal; only supported on MacOS") orelse true;
 
-    const lib = std.Build.CompileStep.create(b, .{
+    const l = std.Build.CompileStep.create(b, .{
         .name = "glfw",
         .kind = .lib,
         .linkage = if (shared) .dynamic else .static,
@@ -21,67 +152,67 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    lib.linkLibC();
+    l.linkLibC();
 
     if (shared) {
-        lib.defineCMacro("_GLFW_BUILD_DLL", "1");
+        l.defineCMacro("_GLFW_BUILD_DLL", "1");
     }
 
-    lib.installHeadersDirectory("include/GLFW", "GLFW");
+    // l.installHeadersDirectory("include/GLFW", "GLFW");
 
-    lib.linkLibrary(b.dependency("vulkan_headers", .{
+    l.linkLibrary(b.dependency("vulkan_headers", .{
         .target = target,
         .optimize = optimize,
     }).artifact("vulkan-headers"));
 
-    if (lib.target_info.target.os.tag == .macos) {
+    if (l.target_info.target.os.tag == .macos) {
         // MacOS: this must be defined for macOS 13.3 and older.
         // Critically, this MUST NOT be included as a -D__kernel_ptr_semantics flag. If it is,
         // then this macro will not be defined even if `defineCMacro` was also called!
-        lib.defineCMacro("__kernel_ptr_semantics", "");
-        @import("xcode_frameworks").addPaths(b, lib);
+        l.defineCMacro("__kernel_ptr_semantics", "");
+        @import("xcode_frameworks").addPaths(b, l);
     }
 
     const include_src_flag = "-Isrc";
 
-    switch (lib.target_info.target.os.tag) {
+    switch (l.target_info.target.os.tag) {
         .windows => {
-            lib.linkSystemLibraryName("gdi32");
-            lib.linkSystemLibraryName("user32");
-            lib.linkSystemLibraryName("shell32");
+            l.linkSystemLibraryName("gdi32");
+            l.linkSystemLibraryName("user32");
+            l.linkSystemLibraryName("shell32");
 
             if (use_opengl) {
-                lib.linkSystemLibraryName("opengl32");
+                l.linkSystemLibraryName("opengl32");
             }
 
             if (use_gles) {
-                lib.linkSystemLibraryName("GLESv3");
+                l.linkSystemLibraryName("GLESv3");
             }
 
             const flags = [_][]const u8{ "-D_GLFW_WIN32", include_src_flag };
-            lib.addCSourceFiles(&base_sources, &flags);
-            lib.addCSourceFiles(&windows_sources, &flags);
+            l.addCSourceFiles(&base_sources, &flags);
+            l.addCSourceFiles(&windows_sources, &flags);
         },
         .macos => {
-            lib.linkSystemLibraryName("objc");
-            lib.linkFramework("IOKit");
-            lib.linkFramework("CoreFoundation");
-            lib.linkFramework("AppKit");
-            lib.linkFramework("CoreServices");
-            lib.linkFramework("CoreGraphics");
-            lib.linkFramework("Foundation");
+            l.linkSystemLibraryName("objc");
+            l.linkFramework("IOKit");
+            l.linkFramework("CoreFoundation");
+            l.linkFramework("AppKit");
+            l.linkFramework("CoreServices");
+            l.linkFramework("CoreGraphics");
+            l.linkFramework("Foundation");
 
             if (use_metal) {
-                lib.linkFramework("Metal");
+                l.linkFramework("Metal");
             }
 
             if (use_opengl) {
-                lib.linkFramework("OpenGL");
+                l.linkFramework("OpenGL");
             }
 
             const flags = [_][]const u8{ "-D_GLFW_COCOA", include_src_flag };
-            lib.addCSourceFiles(&base_sources, &flags);
-            lib.addCSourceFiles(&macos_sources, &flags);
+            l.addCSourceFiles(&base_sources, &flags);
+            l.addCSourceFiles(&macos_sources, &flags);
         },
 
         // everything that isn't windows or mac is linux :P
@@ -93,7 +224,7 @@ pub fn build(b: *std.Build) void {
             sources.appendSlice(&linux_sources) catch unreachable;
 
             if (use_x11) {
-                lib.linkLibrary(b.dependency("x11_headers", .{
+                l.linkLibrary(b.dependency("x11_headers", .{
                     .target = target,
                     .optimize = optimize,
                 }).artifact("x11-headers"));
@@ -103,12 +234,12 @@ pub fn build(b: *std.Build) void {
             }
 
             if (use_wl) {
-                lib.linkLibrary(b.dependency("wayland_headers", .{
+                l.linkLibrary(b.dependency("wayland_headers", .{
                     .target = target,
                     .optimize = optimize,
                 }).artifact("wayland-headers"));
 
-                lib.defineCMacro("WL_MARSHAL_FLAG_DESTROY", "1");
+                l.defineCMacro("WL_MARSHAL_FLAG_DESTROY", "1");
 
                 sources.appendSlice(&linux_wl_sources) catch unreachable;
                 flags.append("-D_GLFW_WAYLAND") catch unreachable;
@@ -117,10 +248,11 @@ pub fn build(b: *std.Build) void {
 
             flags.append(include_src_flag) catch unreachable;
 
-            lib.addCSourceFiles(sources.slice(), flags.slice());
+            l.addCSourceFiles(sources.slice(), flags.slice());
         },
     }
-    b.installArtifact(lib);
+    // b.installArtifact(l);
+    return l;
 }
 
 pub fn addPaths(step: *std.build.CompileStep) void {
@@ -135,65 +267,66 @@ fn sdkPath(comptime suffix: []const u8) []const u8 {
     };
 }
 
+// TODO: remove sdkPath usage below once hexops/mach#902 is fixed.
 const base_sources = [_][]const u8{
-    "src/context.c",
-    "src/egl_context.c",
-    "src/init.c",
-    "src/input.c",
-    "src/monitor.c",
-    "src/null_init.c",
-    "src/null_joystick.c",
-    "src/null_monitor.c",
-    "src/null_window.c",
-    "src/osmesa_context.c",
-    "src/platform.c",
-    "src/vulkan.c",
-    "src/window.c",
+    sdkPath("/src/context.c"),
+    sdkPath("/src/egl_context.c"),
+    sdkPath("/src/init.c"),
+    sdkPath("/src/input.c"),
+    sdkPath("/src/monitor.c"),
+    sdkPath("/src/null_init.c"),
+    sdkPath("/src/null_joystick.c"),
+    sdkPath("/src/null_monitor.c"),
+    sdkPath("/src/null_window.c"),
+    sdkPath("/src/osmesa_context.c"),
+    sdkPath("/src/platform.c"),
+    sdkPath("/src/vulkan.c"),
+    sdkPath("/src/window.c"),
 };
 
 const linux_sources = [_][]const u8{
-    "src/linux_joystick.c",
-    "src/posix_module.c",
-    "src/posix_poll.c",
-    "src/posix_thread.c",
-    "src/posix_time.c",
-    "src/xkb_unicode.c",
+    sdkPath("/src/linux_joystick.c"),
+    sdkPath("/src/posix_module.c"),
+    sdkPath("/src/posix_poll.c"),
+    sdkPath("/src/posix_thread.c"),
+    sdkPath("/src/posix_time.c"),
+    sdkPath("/src/xkb_unicode.c"),
 };
 
 const linux_wl_sources = [_][]const u8{
-    "src/wl_init.c",
-    "src/wl_monitor.c",
-    "src/wl_window.c",
+    sdkPath("/src/wl_init.c"),
+    sdkPath("/src/wl_monitor.c"),
+    sdkPath("/src/wl_window.c"),
 };
 
 const linux_x11_sources = [_][]const u8{
-    "src/glx_context.c",
-    "src/x11_init.c",
-    "src/x11_monitor.c",
-    "src/x11_window.c",
+    sdkPath("/src/glx_context.c"),
+    sdkPath("/src/x11_init.c"),
+    sdkPath("/src/x11_monitor.c"),
+    sdkPath("/src/x11_window.c"),
 };
 
 const windows_sources = [_][]const u8{
-    "src/wgl_context.c",
-    "src/win32_init.c",
-    "src/win32_joystick.c",
-    "src/win32_module.c",
-    "src/win32_monitor.c",
-    "src/win32_thread.c",
-    "src/win32_time.c",
-    "src/win32_window.c",
+    sdkPath("/src/wgl_context.c"),
+    sdkPath("/src/win32_init.c"),
+    sdkPath("/src/win32_joystick.c"),
+    sdkPath("/src/win32_module.c"),
+    sdkPath("/src/win32_monitor.c"),
+    sdkPath("/src/win32_thread.c"),
+    sdkPath("/src/win32_time.c"),
+    sdkPath("/src/win32_window.c"),
 };
 
 const macos_sources = [_][]const u8{
     // C sources
-    "src/cocoa_time.c",
-    "src/posix_module.c",
-    "src/posix_thread.c",
+    sdkPath("/src/cocoa_time.c"),
+    sdkPath("/src/posix_module.c"),
+    sdkPath("/src/posix_thread.c"),
 
     // ObjC sources
-    "src/cocoa_init.m",
-    "src/cocoa_joystick.m",
-    "src/cocoa_monitor.m",
-    "src/cocoa_window.m",
-    "src/nsgl_context.m",
+    sdkPath("/src/cocoa_init.m"),
+    sdkPath("/src/cocoa_joystick.m"),
+    sdkPath("/src/cocoa_monitor.m"),
+    sdkPath("/src/cocoa_window.m"),
+    sdkPath("/src/nsgl_context.m"),
 };
