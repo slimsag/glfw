ece3f2301d36243a3c07da2100b7ae510147dec8
Brody Cooper <81282902+reedthorngag@users.noreply.github.com>
2024-04-30 17:37:04 +1200

    Updated installHeadersDirectory() for latest zig version

--- build.zig
+++ build.zig
@@ -28,7 +28,7 @@ pub fn build(b: *std.Build) void {
 
     if (shared) lib.defineCMacro("_GLFW_BUILD_DLL", "1");
 
-    lib.installHeadersDirectory("include/GLFW", "GLFW");
+    lib.installHeadersDirectory(std.Build.LazyPath { .path = "include/GLFW"}, "GLFW", .{});
     // GLFW headers depend on these headers, so they must be distributed too.
     if (b.lazyDependency("vulkan_headers", .{
         .target = target,
